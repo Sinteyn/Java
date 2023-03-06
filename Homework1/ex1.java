e2e12746dabf2fe50a328107119bf13e61503b64
Darya Vorobeva <d.vorobieva15@gmail.com>
2023-03-06 19:58:52 +0300

    ex1 done

--- Homework1/ex1.java
+++ Homework1/ex1.java
@@ -1,5 +1,28 @@
 package Homework.Homework1;
 
-public class ex1 {
-    
+import java.util.Scanner;
+
+public class ex1
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(reverseWords(scan.nextLine()));
+        scan.close();
+    }
+
+    public static String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        String sentence = s;
+        String[] splitSentense = sentence.split(" ");
+        int len = splitSentense.length;
+
+        for (int i = len - 1; i > 0; i--) {
+            if (!splitSentense[i].equals("")) {
+                result.append(splitSentense[i]).append(" ");
+            }
+
+        }
+        result.append(splitSentense[0]);
+        return result.toString();
+    }
 }
--- Homework1/ex3.java
+++ Homework1/ex3.java
@@ -1,5 +1,1 @@
-package Homework.Homework1;
 
-public class ex3 {
-    
-}
