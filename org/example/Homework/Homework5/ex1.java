ca50420721a9ca5c8630bdcf1b754023f4d415c6
Darya Vorobeva <d.vorobieva15@gmail.com>
2023-03-20 18:18:57 +0300

    ex1 done

--- org/example/Homework/Homework5/ex1.java
+++ org/example/Homework/Homework5/ex1.java
@@ -9,15 +9,15 @@ public class ex1 {
         countNumOccurrencesWords("Россия идет вперед всей планеты. Планета — это не Россия.");
     }
 
-    public static void countNumOccurrencesWords(String text){
+    public static void countNumOccurrencesWords(String text) {
         Map<String, Integer> counter = new HashMap<>();
         String[] resText = text.toLowerCase().split("[-.?!)_(,:— /']");
-        for (String str:resText) {
-            if(str == "") continue;
+        for (String str : resText) {
+            if (str == "") continue;
             counter.putIfAbsent(str, 0);
             counter.put(str, counter.get(str) + 1);
         }
-        for (Map.Entry<String, Integer> entry: counter.entrySet())
+        for (Map.Entry<String, Integer> entry : counter.entrySet())
             System.out.println("Слово \"" + entry.getKey() +
                     "\", встречается --> " + entry.getValue() + " раз(а)");
     }
--- org/example/Homework/Homework5/ex2.java
+++ org/example/Homework/Homework5/ex2.java
@@ -1,4 +1,9 @@
 package org.example.Homework.Homework5;
 
 public class ex2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static
 }
